d2a4a91ba8f2dc22377f741da28ee51a5a7ac443
Muna-glitch <68678829+Muna-glitch@users.noreply.github.com>
2024-03-11 23:06:37 -0400

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,5 +4,14 @@ namespace app\models;
 
 class Post
 {
+    public static function getPosts()
+    {
+        $posts = [
+            ['title' => 'Post 1', 'content' => 'Muna Saed'],
+            ['title' => 'Post 2', 'content' => 'Fordham'],
+            ['title' => 'Post 3', 'content' => 'CISC 3300'],
+        ];
 
+        return $posts;
+    }
 }

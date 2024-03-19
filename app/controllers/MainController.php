f7419a5af66f0d82437df76d946522187a0a5d9b
Muna-glitch <68678829+Muna-glitch@users.noreply.github.com>
2024-03-19 00:04:09 -0400

    Update MainController.php

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -6,22 +6,23 @@ use app\models\Post;
 
 class MainController extends Controller
 {
-    protected $twig;
+
     public function homepage()
     {
         $template = $this->twig->load('main/homepage.twig');
         $homepageData = [
-            'title' => "Jack Forester's Homepage",
+            'title' => 'Homepage Title',
         ];
 
         echo $template->render($homepageData);
     }
 
-    public function notFound() {
-
-        $template = $this->twig->load('notFound/notFound.twig');
+    
+    public function notFound() 
+    {
+        $template = $this->twig->load('error/error.twig');
         http_response_code(404);
         echo $template->render(); 
+    } 
 
-    }   
 }

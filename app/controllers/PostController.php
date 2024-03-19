62b73caf3431e946c2253de658c011af334fdc2a
Muna-glitch <68678829+Muna-glitch@users.noreply.github.com>
2024-03-19 00:05:33 -0400

    Update PostController.php

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -3,20 +3,68 @@
 namespace app\controllers;
 use app\core\Controller;
 use app\models\Post;
+
 class PostController extends Controller
 {
-    public function index()
+    public function submitPosts()
     {
+        include './assets/views/posts/postsView.php';
+    }
 
-        $newPost = new Post();
-        $posts = $newPost->getPosts();
+    public function validatePost()
+    {
+        $username = $_POST['username'] ? $_POST['username'] : false;
+        $title = $_POST['title'] ? $_POST['title'] : false;
+        $description = $_POST['description'] ? $_POST['description'] : false;
+        $errors = [];
+ 
+        if ($username) {
+            $username = htmlspecialchars($username, ENT_QUOTES|ENT_HTML5, 'UTF-8', true);
 
-        $template = $this->twig->load('posts/posts.twig');
-        $postsData = [
-            'posts' => $posts,
+            //validate text length
+            if (strlen($username) < 2) {
+                $errors['usernameShort'] = 'username is too short';
+            }
+        } else {
+            $errors['requiredName'] = 'username is required';
+        }
+
+        //title 
+        if ($title) {
+            $title = htmlspecialchars($title, ENT_QUOTES|ENT_HTML5, 'UTF-8', true);
+
+            //validate text length
+            if (strlen($title) < 2) {
+                $errors['titleShort'] = 'title is too short';
+            }
+        } else {
+            $errors['requiredTitle'] = 'title is required';
+        }
+        if ($description) {
+            $description = htmlspecialchars($description, ENT_QUOTES|ENT_HTML5, 'UTF-8', true);
+
+            //validate text length
+            if (strlen($description) < 2) {
+                $errors['descriptionShort'] = 'description is too short';
+            }
+        } else {
+            $errors['requiredDescription'] = 'description is required';
+        }
+
+        //errors check
+        if (count($errors)) {
+            http_response_code(400);
+            echo json_encode($errors);
+            exit();
+        }
+
+        $returnData = [
+            'name' => $username,
+            'title' => $title,
+            'description' => $description,
         ];
-        echo $template->render($postsData);
+        echo "<span class='form-success'>Post shared successfully!</span>";
+
+        exit();
     }
 }
-
-?>
